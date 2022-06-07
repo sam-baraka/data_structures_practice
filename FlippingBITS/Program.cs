77ab110bca280570376ec77dbe79d9335005fa23
Samuel Baraka <sababuvercetti@gmail.com>
2022-06-07 15:22:02 +0300

    Converted the number to a binary first

---
+++ FlippingBITS/Program.cs
@@ -0,0 +1,52 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'flippingBits' function below.
+     *
+     * The function is expected to return a LONG_INTEGER.
+     * The function accepts LONG_INTEGER n as parameter.
+     */
+
+    public static long flippingBits(long n)
+    {
+        var value=Convert.ToString(n,2);
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+       // TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int q = Convert.ToInt32(Console.ReadLine().Trim());
+
+        for (int qItr = 0; qItr < q; qItr++)
+        {
+            long n = Convert.ToInt64(Console.ReadLine().Trim());
+
+            long result = Result.flippingBits(n);
+
+            Console.WriteLine(result);
+        }
+
+      //  textWriter.Flush();
+        //textWriter.Close();
+    }
+}
