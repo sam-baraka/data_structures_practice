3fd369176a55fa4506e964e7f616626c7658d8ee
Samuel Baraka <sababuvercetti@gmail.com>
2022-08-02 16:54:50 +0300

    Finished counting sort

---
+++ CountingSort/Program.cs
@@ -0,0 +1,61 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'countingSort' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static List<int> countingSort(List<int> arr)
+    {
+        int[] result = new int[100];
+
+        // take the input List<int> and break it into chunks of 100
+        for (int i = 0; i < arr.Count; i += 100)
+        {
+            for (int j = i; j < i + 100; j++)
+            {
+                // the int value is the result array index
+                // increment the count at that index
+                result[arr[j]]++;
+            }
+        }
+
+        return result.ToList();
+
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+       
+
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+        List<int> result = Result.countingSort(arr);
+
+        Console.WriteLine(String.Join(" ", result));
+
+    }
+}
