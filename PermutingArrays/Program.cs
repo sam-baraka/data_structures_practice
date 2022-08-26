22a7c58ede3054d75942e9101293d80df130618b
Samuel Baraka <sababuvercetti@gmail.com>
2022-08-26 15:33:52 +0300

    fixed git ignore

--- PermutingArrays/Program.cs
+++ PermutingArrays/Program.cs
@@ -27,7 +27,15 @@ class Result
 
     public static string twoArrays(int k, List<int> A, List<int> B)
     {
+        A.Sort();
+        B.Sort();
+        B.Reverse();
 
+        for (int i = 0; i < A.Count; i++)
+        {
+            if (A[i] + B[i] < k) return "NO";
+        }
+        return "YES";
     }
 
 }
